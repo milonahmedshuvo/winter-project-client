3e31cbd792c8df3af0c8fd68ca083e4ce446e717
milonahmedshuvo <milonahmedshuvo01@gmail.com>
2023-10-06 23:50:25 +0600

    add commits

--- src/Sheare/Navbar/Navbar.jsx
+++ src/Sheare/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
 
   return (
     <div>
-       <h1 className='bg-purple-400'>navbar daisi ui  </h1>  
+       <h1 className='bg-purple-400'>navbar daisi ui json file add  </h1>  
     </div>
   )
 }
