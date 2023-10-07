47eb2869795fe451b212bf5216edc1e37ca3a263
milonahmedshuvo <milonahmedshuvo01@gmail.com>
2023-10-07 23:52:46 +0600

    navber

--- src/Sheare/Navbar/Navbar.jsx
+++ src/Sheare/Navbar/Navbar.jsx
@@ -1,15 +1,45 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from "react";
+import logo from '../../image/logo.png'
+import { FaSistrix, FaRegUser,FaRegHeart,FaRegTrashAlt } from 'react-icons/fa';
 
 const Navbar = () => {
-
-
-  
-
   return (
-    <div>
-       <h1 className='bg-purple-400'>navbar daisi ui json file add  </h1>  
-    </div>
-  )
-}
+    <div className="mt-[33px] border-red-400 border">
+         <div className="flex" >
+               <div className="image">
+                <img src={logo} alt="" className="w-[120px] h-[50px]" />
+               </div>
 
-export default Navbar
+               {/* search */}
+               <div className="search flex">
+               <input type="text" placeholder="Im looking for.." className=" border w-[713px] h-[50px]  " />
+               <div className="w-[65px] bg-[#0198E9] h-[50px] flex justify-center items-center rounded-tr-[5px] rounded-br-[5px] ">
+                <FaSistrix className='text-white text-2xl'></FaSistrix>
+               </div>
+               </div>
+
+               {/* icons  */}
+               <div className="icons flex ">
+                 <div className="w-[73px] h-[24px] flex items-center bg-red-400" >
+                  <FaRegUser></FaRegUser>
+                  <p>Login</p>
+                 </div>
+
+                 <div className="w-[91px] h-[24px] flex items-center bg-green-300">
+                  <FaRegHeart></FaRegHeart>
+                  <p>Wishlist</p>
+                 </div>
+
+                 <div className="w-[80px] h-[20px] flex items-center bg-yellow-300">
+                  <FaRegTrashAlt></FaRegTrashAlt>
+                  <p>My card</p>
+                 </div>
+               </div>
+
+
+         </div>
+    </div>
+  );
+};
+
+export default Navbar;
