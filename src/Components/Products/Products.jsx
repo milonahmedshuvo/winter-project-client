c1a358dd6decc276345eb23db3af8fa6d89ad32d
milonahmedshuvo <milonahmedshuvo01@gmail.com>
2023-10-10 17:09:41 +0600

    card responsive

--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -1,38 +1,32 @@
-import React, { useEffect, useState } from 'react'
-import SingleProduct from './SingleProduct'
+import React, { useEffect, useState } from "react";
+import SingleProduct from "./SingleProduct";
 // import fristProducts from '../../../fristproducts.json'
-import data from '../../data'
+import data from "../../data";
+
 
 
 const Products = () => {
-const [datas, setDatas] = useState([])
+  const [datas, setDatas] = useState([]);
 
-useEffect(()=> {
-    fetch('../../../public/fristproducts.json')
-    .then((res) => res.json())
-    .then((data) => setDatas(data))
-    .catch((err) => console.log(err))
-},[])
-
-
-
-
-
-
-
-
+  useEffect(() => {
+    fetch("../../../public/fristproducts.json")
+      .then((res) => res.json())
+      .then((data) => setDatas(data))
+      .catch((err) => console.log(err));
+  }, []);
 
   return (
-    <div className='grid grid-cols-2 lg:grid-cols-4'>
-
-        {
-            data?.map((product) => <SingleProduct
-            key={product.id}
-            product={product}
-            > </SingleProduct> )
-        }
+    <div className="mt-16 mx-4">
+      <p className="text-[16px] md:text-[26px] frontFive text-[#2E2E2E]"> Flash Deals</p>
+      <div className="grid grid-cols-2 lg:grid-cols-4 gap-3 md:gap-0">
+        {data?.map((product) => (
+          <SingleProduct key={product.id} product={product}>
+            {" "}
+          </SingleProduct>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Products
+export default Products;
--- src/Components/Products/SingleProduct.jsx
+++ src/Components/Products/SingleProduct.jsx
@@ -1,44 +1,52 @@
-import React, { useContext } from 'react'
+import React, { useContext } from "react";
 import { FaStar, FaRegHeart } from "react-icons/fa";
-import basket from '../../image/basket.png'
-import { Link } from 'react-router-dom';
-import { MyContext } from '../../../Context';
+import basket from "../../image/basket.png";
+import { Link } from "react-router-dom";
+import { MyContext } from "../../../Context";
 
-
-
-const SingleProduct = ({product}) => {
-const {setValueOne} = useContext(MyContext)
- const {image, title, price, id} = product;
+const SingleProduct = ({ product }) => {
+  const { setValueOne } = useContext(MyContext);
+  const { image, title, price, id } = product;
 
   return (
-    <div className='border my-14 border-[##E9E9E9] rounded w-[332px] h-[469px] p-1 relative ' >
-       <Link to={`/productDatails/${id}`} onClick={() =>setValueOne(id)} >
-       <div className='relative'>
-            <img className='h-[313px] w-full rounded' src={image} alt="" />
+    <div className="border  mt-5 border-[##E9E9E9] rounded w-full md:w-[332px] h-[469px] p-1 flex flex-col justify-between ">
+      <div>
+        <Link to={`/productDatails/${id}`} onClick={() => setValueOne(id)}>
+          <div className="relative">
+            <img className="h-[313px] w-full rounded" src={image} alt="" />
 
-            <FaRegHeart className='text-[30px] text-[#999999] absolute top-4 right-3'></FaRegHeart>
-        </div>
-       </Link>
+            <FaRegHeart className=" text-[22px] md:text-[30px] text-[#999999] absolute top-4 right-3"></FaRegHeart>
+          </div>
+        </Link>
 
-        <p className='w-24 flex my-2 '>
-        <FaStar className='text-[#FAC96B]' ></FaStar> 
-        <FaStar className='text-[#FAC96B]' ></FaStar> 
-        <FaStar className='text-[#FAC96B]' ></FaStar> 
-        <FaStar className='text-[#FAC96B]' ></FaStar> 
-        <FaStar className='text-[#D3D3D3]' ></FaStar> 
-          
-          
+        <p className="w-24 flex my-2 ">
+          <FaStar className="text-[#FAC96B]"></FaStar>
+          <FaStar className="text-[#FAC96B]"></FaStar>
+          <FaStar className="text-[#FAC96B]"></FaStar>
+          <FaStar className="text-[#FAC96B]"></FaStar>
+          <FaStar className="text-[#D3D3D3]"></FaStar>
         </p>
-        <p className='text-[#383838] text-[18px] styleFront' >{title}</p>
+        <p className="text-[#383838] text-[14px] md:text-[18px] styleFront">
+          {title}
+        </p>
+      </div>
 
-        <div className='absolute bottom-3'>
+      {/* <div className='absolute bottom-3'>
         <div className='grid grid-cols-2'>
             <p className='text-[22px] text-[#0970CD] frontFive ' >${price}</p>
             <p className='w-6 mx-[129px]' >  <img src={basket} alt="" /></p>
         </div>
-        </div>
-    </div>
-  )
-}
+        </div> */}
 
-export default SingleProduct
+      <div className="flex justify-between w-full ">
+        <p className=" text-[16px] md:text-[22px] text-[#0970CD] frontFive ">${price}</p>
+        <p className=" w-5 md:w-6">
+          
+          <img src={basket} alt="" />
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default SingleProduct;
