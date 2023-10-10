ad231200a978366dec657bca8c10c01901190685
milonahmedshuvo <milonahmedshuvo01@gmail.com>
2023-10-11 03:58:03 +0600

    products datails

--- src/Components/Products/DatailsPage.jsx
+++ src/Components/Products/DatailsPage.jsx
@@ -1,6 +1,9 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { MyContext } from '../../../Context'
 import products from '../../data'
+import { FaStar, FaRegHeart, FaCheck } from "react-icons/fa";
+import { useNavigate } from 'react-router-dom';
+
 
 
 
@@ -8,13 +11,90 @@ const DatailsPage = () => {
 const {valueOne} = useContext(MyContext)
 console.log("ddddddd", valueOne)
 
-const product = products.find(pro => pro.id == valueOne)
+
+
+const product = products?.find(pro => pro.id == valueOne)
 console.log(product)
-const {image, title,  discription, catagoryImage, price, } = product
+const {image, title,  discription, catagoryImage, price, option } = product
+
+
+ const [images, setImage] = useState(image)
+
+
 
   return (
     <div>
       <h1>datails page</h1>  
+
+
+          <div className='grid grid-cols-1 md:grid-cols-2 gap-10 '>
+
+              <div className="image border border-purple-400 w-full flex justify-between">
+                   <div>
+                    <img src={images} className='w-[519px] border border-[#F4F4F6]' alt="" />
+                   </div>
+
+                   <div className='mt-3 flex flex-col'>
+                       {
+                        catagoryImage.map((img, i) => <button onClick={() => setImage(img)} key={i} className='focus:border border-[#21A2F8]'>   <img src={img}  className='my-3  w-[104px] h-[104px] ' ></img>  </button> )
+                       }
+                   </div>
+              </div>
+
+
+
+
+
+              <div className="allDatails border border-green-300 w-[661px] pr-6">
+                <p className='text-[26px] text-[#333333] frontFive ' >{title}</p>
+                 <div className='flex justify-between items-center'>
+                 <p className=" flex items-center my-2 ">
+                  <p className='frontFive mr-1'>4.8</p>
+          <FaStar className="text-[#FAC96B]"></FaStar>
+          <FaStar className="text-[#FAC96B]"></FaStar>
+          <FaStar className="text-[#FAC96B]"></FaStar>
+          <FaStar className="text-[#FAC96B]"></FaStar>
+          <FaStar className="text-[#D3D3D3]"></FaStar>
+          <p className='text-[#BABABA] text-[18px] ml-1 '>(226)</p>
+        </p>
+
+        <p className='flex items-center'> <FaCheck className='text-green-400'></FaCheck> <span className='text-[14px] ml-2 text-[#333333] styleFront '> 4,320 sold</span> </p>
+        <p className='flex items-center' > <FaRegHeart></FaRegHeart> <span className='text-[#0198E9] styleFront ml-4 '>Add to wishlist</span></p>
+                 </div>
+
+
+                  <div className="price mt-8 flex items-center">
+                      <p className='text-[#0198E9] text-[30px]'>${price} 
+                        
+                      </p>
+                      <p className='ml-4 line-through  text-[#8D8D8D]'>$1,020.99</p>
+                  </div>
+
+                  <div className="sku">
+                    <p>
+                    SKU: 12314124124
+                    <span>In stock</span>
+                    </p>
+                  </div>
+
+
+                  <div className="drescription">
+                       <p>{discription}</p>
+                  </div>
+
+                  <div className="option ml-10">
+                     {
+                       option.map((list, i) => <ul style={{listStyleType: 'disc'}}
+                       key={i}
+                       >
+                        <li> {list} </li>
+                       </ul>)
+                     }
+                  </div>
+
+              </div>
+          </div>
+
     </div>
   )
 }
--- src/data.jsx
+++ src/data.jsx
@@ -11,7 +11,7 @@ const datas =
     { "id": "1",
       "image": picone,
       "title": "New Arrivals Factory Custom Classic Ladies",
-      "catagoryImage": ["https://s.alicdn.com/@sc04/kf/HLB1cGyPTgHqK1RjSZJnq6zNLpXa7.jpg_720x720q50.jpg", "https://s.alicdn.com/@sc04/kf/HLB1YW67afc3T1VjSZLeq6zZsVXan.jpg_100x100xz.jpg", "https://s.alicdn.com/@sc04/kf/HLB1F7eJTbvpK1RjSZFqq6AXUVXaI.jpg_100x100xz.jpg", "https://s.alicdn.com/@sc04/kf/HLB1YW67afc3T1VjSZLeq6zZsVXan.jpg_100x100xz.jpg"],
+      "catagoryImage": [picone, "https://s.alicdn.com/@sc04/kf/HLB1YW67afc3T1VjSZLeq6zZsVXan.jpg_100x100xz.jpg", "https://s.alicdn.com/@sc04/kf/HLB1F7eJTbvpK1RjSZFqq6AXUVXaI.jpg_100x100xz.jpg", "https://s.alicdn.com/@sc04/kf/HLB1YW67afc3T1VjSZLeq6zZsVXan.jpg_100x100xz.jpg"],
       "discription": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
       "option": [
         "Direct Full Array",
@@ -25,7 +25,7 @@ const datas =
     //   "image": "https://s.alicdn.com/@sc04/kf/HTB1J6YWuyMnBKNjSZFoq6zOSFXad.jpg_100x100xz.jpg",
       "image": pictwo,
       "title": "Comfortable light spring autumn",
-      "catagoryImage": ["https://s.alicdn.com/@sc04/kf/HTB1J6YWuyMnBKNjSZFoq6zOSFXad.jpg_100x100xz.jpg", "https://s.alicdn.com/@sc04/kf/HTB1gsrxKf5TBuNjSspcq6znGFXa6.jpg_100x100xz.jpg", "https://s.alicdn.com/@sc04/kf/HTB1kDXoB5CYBuNkHFCcq6AHtVXaL.jpg_100x100xz.jpg", "https://s.alicdn.com/@sc04/kf/HTB1gsrxKf5TBuNjSspcq6znGFXa6.jpg_100x100xz.jpg"],
+      "catagoryImage": [pictwo, "https://s.alicdn.com/@sc04/kf/HTB1gsrxKf5TBuNjSspcq6znGFXa6.jpg_100x100xz.jpg", "https://s.alicdn.com/@sc04/kf/HTB1kDXoB5CYBuNkHFCcq6AHtVXaL.jpg_100x100xz.jpg", "https://s.alicdn.com/@sc04/kf/HTB1gsrxKf5TBuNjSspcq6znGFXa6.jpg_100x100xz.jpg"],
       "discription": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
       "option": [
         "Direct Full Array",
@@ -39,7 +39,7 @@ const datas =
     //   "image": "https://s.alicdn.com/@sc04/kf/H911b84a919ef41e5b17a421168e97620e.jpg_100x100xz.jpg",
       "image": pictree,
       "title": "Winter Latest Designs Lotus Leaf Hem Women",
-      "catagoryImage": ["https://s.alicdn.com/@sc04/kf/H911b84a919ef41e5b17a421168e97620e.jpg_100x100xz.jpg", "https://s.alicdn.com/@sc04/kf/H12e3a2e1f6044b468b500999f875918fc.jpg_100x100xz.jpg", "https://s.alicdn.com/@sc04/kf/H9a14406be6d9494685b3ea1c713115edr.jpg_100x100xz.jpg", "https://s.alicdn.com/@sc04/kf/H12e3a2e1f6044b468b500999f875918fc.jpg_100x100xz.jpg"],
+      "catagoryImage": [pictree, "https://s.alicdn.com/@sc04/kf/H12e3a2e1f6044b468b500999f875918fc.jpg_100x100xz.jpg", "https://s.alicdn.com/@sc04/kf/H9a14406be6d9494685b3ea1c713115edr.jpg_100x100xz.jpg", "https://s.alicdn.com/@sc04/kf/H12e3a2e1f6044b468b500999f875918fc.jpg_100x100xz.jpg"],
       "discription": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
       "option": [
         "Direct Full Array",
@@ -53,7 +53,7 @@ const datas =
     //   "image": "https://s.alicdn.com/@sc04/kf/HTB1ZUz8asfrK1Rjy0Fmq6xhEXXab.jpg_100x100xz.jpg",
       "image": picfrour,
       "title": "New Arrivals Factory Custom Classic Ladies",
-      "catagoryImage": ["https://s.alicdn.com/@sc04/kf/HTB1tCCTaizxK1RjSspjq6AS.pXae.jpg_100x100xz.jpg", "https://s.alicdn.com/@sc04/kf/HTB19XnqJ1OSBuNjy0Fdq6zDnVXa5.jpg_100x100xz.jpg", "https://s.alicdn.com/@sc04/kf/HTB1cKbcBRmWBuNkSndVq6AsApXaH.jpg_100x100xz.jpg", "https://s.alicdn.com/@sc04/kf/HTB1D4cGByCYBuNkHFCcq6AHtVXaz.jpg_100x100xz.jpg"],
+      "catagoryImage": [picfrour, "https://s.alicdn.com/@sc04/kf/HTB19XnqJ1OSBuNjy0Fdq6zDnVXa5.jpg_100x100xz.jpg", "https://s.alicdn.com/@sc04/kf/HTB1cKbcBRmWBuNkSndVq6AsApXaH.jpg_100x100xz.jpg", "https://s.alicdn.com/@sc04/kf/HTB1D4cGByCYBuNkHFCcq6AHtVXaz.jpg_100x100xz.jpg"],
       "discription": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
       "option": [
         "Direct Full Array",
