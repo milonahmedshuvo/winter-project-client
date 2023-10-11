ade7419ff38734c33ecdc5bb5f169ccae053bdc3
milonahmedshuvo <milonahmedshuvo01@gmail.com>
2023-10-12 03:39:09 +0600

    datails page

--- src/Components/Products/DatailsPage.jsx
+++ src/Components/Products/DatailsPage.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from 'react'
 import { MyContext } from '../../../Context'
 import products from '../../data'
-import { FaStar, FaRegHeart, FaCheck } from "react-icons/fa";
+import { FaStar, FaRegHeart, FaCheck, FaRegWindowMinimize, FaPlus } from "react-icons/fa";
 import { useNavigate } from 'react-router-dom';
 
 
@@ -70,27 +70,106 @@ const {image, title,  discription, catagoryImage, price, option } = product
                       <p className='ml-4 line-through  text-[#8D8D8D]'>$1,020.99</p>
                   </div>
 
-                  <div className="sku">
-                    <p>
+                  <div className="sku flex items-center mt-2 ">
+                    <p className='text-[#757575] styleFront text-[16px]'>
                     SKU: 12314124124
-                    <span>In stock</span>
+            
                     </p>
+                    
+                    <p className='flex items-center ml-2'>
+                      <div className='h-[10px] w-[10px]  text-white rounded-full flex items-center bg-[#3AB137]'>
+                      <FaCheck className='text-[7px] '></FaCheck>
+                      </div>
+                   
+                    <span className='text-[#757575] ml-1 styleFront '>In stock</span>
+                    </p>
+
                   </div>
 
 
-                  <div className="drescription">
+                  <div className="drescription text-[#333333] styleFront leading-[30px] text-[18px] mt-6 ">
                        <p>{discription}</p>
                   </div>
 
-                  <div className="option ml-10">
+                  <div className="option ml-10 mt-8">
                      {
-                       option.map((list, i) => <ul style={{listStyleType: 'disc'}}
+                       option.map((list, i) => <ul className='text-[#333333] text-[16px] mt-1 styleFront ' style={{listStyleType: 'disc'}}
                        key={i}
                        >
                         <li> {list} </li>
                        </ul>)
                      }
                   </div>
+
+                  <p className='w-[357px] h-[29px] flex justify-between mt-6 '>
+                      <p className='border border-[ #EAEAEA] p-[4px, 13px, 4px, 13px] text-[14px] rounded  w-[33px] h-[29px]  flex items-center justify-center mr-3 text-[#6E6E6F] bg-[#FFFFFF] '>
+                        size
+                      </p>
+
+                      <p className='border border-[ #EAEAEA] p-[4px, 13px, 4px, 13px] text-[9px] rounded w-[33px] h-[29px] flex items-center justify-center mr-3 text-[#6E6E6F] bg-[#FFFFFF] '>
+                        S
+                      </p>
+
+                      <p className='border border-[ #EAEAEA] p-[4px, 13px, 4px, 13px] text-[9px] rounded bg-[#0198E9] w-[33px]  h-[29px] flex items-center justify-center mr-3  text-white '>
+                        M
+                      </p>
+                      <p className='border border-[ #EAEAEA] p-[4px, 13px, 4px, 13px] text-[9px] rounded w-[33px] h-[29px] flex items-center justify-center mr-3 text-[#6E6E6F] bg-[#FFFFFF] '>
+                        L
+                      </p>
+
+                      <p className='border border-[ #EAEAEA] p-[4px, 13px, 4px, 13px] text-[9px] rounded w-[33px] h-[29px] flex items-center justify-center mr-3 text-[#6E6E6F] bg-[#FFFFFF] '>
+                        X
+                      </p>
+
+                      <p className='border border-[ #EAEAEA] p-[4px, 13px, 4px, 13px] text-[9px] rounded w-[33px] h-[29px] flex items-center justify-center mr-3 text-[#6E6E6F] bg-[#FFFFFF] '>
+                        XL
+                      </p>
+                      <p className='border border-[ #EAEAEA] p-[4px, 13px, 4px, 13px] text-[9px] rounded w-[33px]  h-[29px] flex items-center justify-center mr-3 text-[#6E6E6F] bg-[#FFFFFF] '>
+                        XXL
+                      </p>
+                  </p>
+
+
+
+                  <p className='flex justify-start items-center w-[600px] mt-6 '>
+
+                      <p className='flex items-center'>
+                        <span className='text-[14px] text-[#3D3D3F] styleFront mr-1'>Quantity: </span>
+
+
+                        <p className='flex items-center justify-start'>
+
+                          
+                          <p className='border border-[ #EAEAEA] p-[4px, 13px, 4px, 13px] text-[9px] rounded w-[33px]  h-[29px] flex items-center justify-center mr-3 text-[#6E6E6F] bg-[#FFFFFF] '>
+                          <FaRegWindowMinimize></FaRegWindowMinimize>
+                        </p>
+                        <p className='border border-[ #EAEAEA] p-[4px, 13px, 4px, 13px] text-[9px] rounded w-[33px]  h-[29px] flex items-center justify-center mr-3 text-[#6E6E6F] bg-[#FFFFFF] '>
+                         2
+                        </p>
+                        
+                        <p className='border border-[ #EAEAEA] p-[4px, 13px, 4px, 13px] text-[9px] rounded w-[33px]  h-[29px] flex items-center justify-center mr-3 text-[#6E6E6F] bg-[#FFFFFF] '>
+                        <FaPlus></FaPlus> 
+                        </p>
+
+                        
+
+                        </p>
+
+                      </p>
+
+
+
+                      <div className='flex gap-5'>
+                        <input type="button" className='text-[#FFFFFF] text-[16px] frontFive w-[165px] h-[35px] bg-[#0198E9] rounded ' value="Add to card" />
+                        
+                        <input type="button" className='text-[#0198E9] text-[16px] frontFive w-[165px] h-[35px] border border-[#0198E9] rounded' value="Add to card" />
+                      </div>
+
+
+
+
+                  </p>
+
 
               </div>
           </div>
